ed497b0352aa435acdb34197900b1c2d04082dca
halee-t <haleetisler@gmail.com>
2025-01-17 01:10:55 -0500

    edits to page.tsx, adding Navbar and Hero components, deleting Landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
 import About from "@/components/about";
 import Landing from "@/components/landing";
 import Image from "next/image";
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
+
+import { useEffect } from "react";
 
 export default function Home() {
   return (
-    <div>
+    <div className="mx-4 md:mx-md lg:mx-lg 2xl:mx-2xl text-primaryText">
+      {/*
       <Landing />
       <About />
+      */}
+      <Navbar />
+      <Hero />
     </div>
   );
 }
