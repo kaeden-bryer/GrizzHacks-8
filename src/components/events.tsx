f0436239ab672cd416ac4acd449bd4afe352cea9
Bdtranter <bdtranter@oakland.edu>
2025-01-17 21:27:03 -0500

    commented out previous schedule header and added a header to match the rest of the page

--- src/components/events.tsx
+++ src/components/events.tsx
@@ -2,8 +2,14 @@ import EventDay from '../components/EventDay';
 import SectionHeader from './SectionHeader';
 export default function events() {
     return(
-	<section id="schedule" className="containerr my-10 oultine outline-white">
-		<div className=" flex justify-center mb-5"><SectionHeader title={'Schedule'} /></div>
+	<section id="schedule" className="containerr my-10 oultine outline-white justify-center">
+
+		<h1 className="flex justify-center mb-5 bg-gradient-to-b from-primaryBlueStart to-primaryBlueEnd z-20 bg-clip-text text-center text-5xl py-2 text-transparent font-bold lg:text-7xl">
+          Schedule
+        </h1>
+		{/*
+		<div className=" flex justify-center w-full mb-5"><SectionHeader title={'Schedule'} /></div>
+		*/}
 		<EventDay />
 	</section>
 );
