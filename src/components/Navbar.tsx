2711f2dd72aa3529dc518282697efb2e4ca9ffe7
halee-t <haleetisler@gmail.com>
2025-01-21 17:46:09 -0500

    more nav fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar: React.FC = () => {
   return (
     <>
       {/* MLH banner */}
-      <div className="md:w-[3.5rem] w-[4.5rem] absolute top-[4rem] md:top-[6.5rem] lg:top-0 2xl:right-10 xl:right-5 lg:right-2 right-[3rem] z-20">
+      <div className="w-[3.5rem] lg:w-[4.5rem] absolute top-[6rem] md:top-[6.5rem] lg:top-0 2xl:right-10 xl:right-5 lg:right-2 md:right-[3rem] right-[1rem] z-20">
         <a
           id="mlh-trust-badge"
           href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2025-season&utm_content=black"
@@ -24,12 +24,12 @@ const Navbar: React.FC = () => {
       </div>
 
       {/* Nav Bar */}
-      <nav className="top-10 z-10 w-full h-[4rem] md:h-[5rem] text-blackColor bg-white rounded-lg flex justify-between items-center md:px-10 sm:px-5 py-3 mb-4 md:mt-6 sm:mt-4 px-4">
+      <nav className="top-10 z-10 w-full h-[5rem] text-blackColor bg-white rounded-lg flex justify-between items-center md:px-10 sm:px-5 py-3 mb-4 md:mt-6 sm:mt-4 px-4">
         <a href="#home">
           <img
             src="/images/logo.svg"
             alt="Grizzhacks"
-            className="object-cover md:w-16 w-12"
+            className="object-cover md:w-16 w-14"
           />
         </a>
         <div className="lg:flex justify-center items-center gap-7 text-2xl hidden">
