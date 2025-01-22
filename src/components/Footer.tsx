56acef402e594523fad816db82e1020df3b8f160
halee-t <haleetisler@gmail.com>
2025-01-22 15:57:21 -0500

    footer lighter changes, updating icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,25 +3,30 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <div className="flex justify-between py-3 mt-5 items-center containerr px-10 bg-neutral-900">
-        <a href="/" className="md:text-2xl text-lg text-white">
+      <div className="flex justify-between py-3 mt-5 items-center containerr px-10 bg-navWhite text-blackColor mt-24">
+        <a href="/" className="md:text-2xl text-lg">
           &copy; GrizzHacks {new Date().getFullYear()}
         </a>
+        <a
+          href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf"
+          target="_blank"
+        >
+          <h2 className="underline underline-offset-2 hover:underline-offset-4">
+            MLH Code of Conduct
+          </h2>
+        </a>
         <div className="flex gap-2 items-center">
+          <a target="_blank" href="mailto:grizzhacksou@gmail.com">
+            <img className="w-6 md:w-10" src="/email-dark.svg" alt="" />
+          </a>
+          <a target="_blank" href="https://www.instagram.com/grizzhacks/">
+            <img className="w-6 md:w-10" src="/insta-dark.svg" alt="" />
+          </a>
           <a
             target="_blank"
             href="https://www.linkedin.com/company/grizzhacks/?viewAsMember=true"
           >
-            <img className="w-6 md:w-10" src="/images/linkedin.svg" alt="" />
-          </a>
-          <a target="_blank" href="https://twitter.com/GrizzHacks">
-            <img className="w-6 md:w-10" src="/images/twitter.svg" alt="" />
-          </a>
-          <a target="_blank" href="https://www.tiktok.com/@grizzhacks">
-            <img className="w-6 md:w-10" src="/images/tiktok.svg" alt="" />
-          </a>
-          <a target="_blank" href="https://www.instagram.com/grizzhacks/">
-            <img className="w-6 md:w-10" src="/images/instagram.svg" alt="" />
+            <img className="w-6 md:w-10" src="/linkedin-dark.svg" alt="" />
           </a>
         </div>
       </div>
