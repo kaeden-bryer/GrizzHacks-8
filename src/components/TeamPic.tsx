6629e02e309f44cfce9c31e14165b7daf187bf7d
halee-t <haleetisler@gmail.com>
2025-01-22 01:59:12 -0500

    Team format changes

--- src/components/TeamPic.tsx
+++ src/components/TeamPic.tsx
@@ -11,33 +11,35 @@ interface TeamPicProps {
 const TeamPic: React.FC<TeamPicProps> = ({ source, linkedIn, name, title }) => {
   const [hovered, setHovered] = useState(false);
   return (
-    <div className="flex flex-col items-center gap-0.5 relative">
-      <div className="absolute inset-0 bg-neutral-950/10 rounded-full hidden xl:block"></div>
-      <a
-        href={linkedIn}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="relative xl:w-44 xl:h-44 lg:w-36 lg:h-36 w-16 h-16 object-cover rounded-full cursor-pointer ring-2 ring-primary ring-opacity-40"
-        onMouseEnter={() => setHovered(true)}
-        onMouseLeave={() => setHovered(false)}
-      >
-        <img
-          src={source}
-          alt={name}
-          className="absolute inset-0 rounded-full w-full h-full object-cover object-top -z-10"
-        />
-        {hovered && (
-          <div className="hidden lg:flex h-1/2 w-full absolute bottom-0 bg-gradient-to-b from-transparent to-primaryBlueDark opacity-90 z-10 rounded-b-full justify-center items-end pb-3">
-            <div className="flex gap-2 justify-center items-center">
-              <p className="xl:text-2xl text-white lg:text-xl text-lg">
-                {name}
-              </p>
-            </div>
-          </div>
-        )}
-      </a>
-      <p className="lg:hidden">{name}</p>
-    </div>
+    <>
+      <div className="flex flex-col justify-center gap-4 relative z-10">
+        <div className="bg-yellow absolute top-0 -z-20 w-64 h-72 rounded-xl"></div>
+        <a
+          href={linkedIn}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="relative top-4 xl:w-52 xl:h-52 lg:w-36 lg:h-36 w-16 h-16 object-cover rounded-lg cursor-pointer mx-auto"
+          onMouseEnter={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
+        >
+          <img
+            src={source}
+            alt={name}
+            className="relative rounded-lg w-full h-full overflow-hidden object-cover object-top -z-10"
+          />
+          <img
+            src="/images/linkedin.svg"
+            className="absolute -bottom-5 -right-5 scale-[60%]"
+          />
+        </a>
+        <div className="flex flex-col items-start w-64">
+          <h2 className="px-5 font-semibold text-blackColor text-xl mt-1">
+            {name}
+          </h2>
+          <p className="px-5 text-blackColor text-base -mt-1">{title}</p>
+        </div>
+      </div>
+    </>
   );
 };
 
