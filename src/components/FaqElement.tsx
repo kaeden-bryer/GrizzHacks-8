d1cccc1dc9a7989b07749e01122b9480d47826a7
halee-t <haleetisler@gmail.com>
2025-01-23 03:11:33 -0500

    new faq design finished

--- src/components/FaqElement.tsx
+++ src/components/FaqElement.tsx
@@ -24,38 +24,48 @@ const FaqElement: React.FC<FaqElementProps> = ({
 
   return (
     <>
-      <div
-        className={`rounded-md ${
-          active ? "bg-neutral-950/60" : "bg-neutral-950/80"
-        } hover:bg-neutral-900/40 flex flex-col justify-start p-3`}
-      >
-        {/* Clickable Header */}
-        <div
-          className="flex justify-between cursor-pointer"
-          onClick={onToggle}
-          role="button"
-          tabIndex={0}
-        >
-          <h5 className="xl:text-2xl md:text-xl text-lg text-white font-semibold">
-            {question}
-          </h5>
-          <img
-            src="/images/arrow.svg"
-            alt="Expand"
-            className={`w-4 transition-all duration-300 ${
-              active ? "rotate-90" : ""
-            }`}
-          />
-        </div>
-        {/* Expandable Content */}
+      <div className="relative">
+        {/* duck */}
         {active && (
-          <div className="w-fit">
-            <p
-              className="my-1 text-white xl:text-lg"
-              dangerouslySetInnerHTML={{ __html: sanitizedAnswer }}
-            ></p>
+          <div className="absolute -top-[5rem] right-0 w-20">
+            <img src="/faqduck.svg" alt="" className="w-full h-auto" />
           </div>
         )}
+
+        <div
+          className={`rounded-md text-blackColor ring-1 ring-primary ${
+            active ? "bg-faqColor" : "bg-navWhite"
+          } hover:bg-faqColor flex flex-col justify-start p-3`}
+        >
+          {/* Clickable Header */}
+          <div
+            className="flex justify-between cursor-pointer"
+            onClick={onToggle}
+            role="button"
+            tabIndex={0}
+          >
+            <h5 className="xl:text-2xl md:text-xl text-lg font-semibold">
+              {question}
+            </h5>
+            <img
+              src="/faq-arrow.svg"
+              alt="Expand"
+              className={`w-5 transition-all duration-300 ${
+                active ? "rotate-90" : ""
+              }`}
+            />
+          </div>
+
+          {/* Expandable Content */}
+          {active && (
+            <div className="w-fit">
+              <p
+                className="my-1 xl:text-lg"
+                dangerouslySetInnerHTML={{ __html: sanitizedAnswer }}
+              ></p>
+            </div>
+          )}
+        </div>
       </div>
     </>
   );
