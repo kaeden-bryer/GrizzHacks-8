5362a7a50fed224f3070af6c0b2f425028d921cd
halee-t <haleetisler@gmail.com>
2025-01-24 20:33:34 -0500

    fixing build errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import About from "@/components/about";
+//import About from "@/components/about";
 import Landing from "@/components/landing";
 
 export default function Home() {
   return (
     <div>
       <Landing />
-      <About />
+      {/*<About />*/}
     </div>
   );
 }
