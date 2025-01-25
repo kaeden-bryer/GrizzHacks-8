15f62b72c329525f9f90a09ab2a0059e61106bfa
halee-t <haleetisler@gmail.com>
2025-01-24 20:06:36 -0500

    sponsor changes

--- src/components/SponsorGrid.tsx
+++ src/components/SponsorGrid.tsx
@@ -6,26 +6,11 @@ const SponsorGrid = () => {
   return (
     <>
       {/* LARGE SCREEN SIZE */}
-      <div className="hidden xl:grid grid-cols-5 grid-rows-3 gap-2 p-4">
-        <a
-          href="mailto:grizzhacksou@gmail.com"
-          className=" text-blackColor flex flex-col items-center mt-16 p-6 col-span-5 row-span-1 ring-2 ring-blackColor rounded-md bg-gradient-to-bl from-primaryBlueEnd via-primaryBlueStart to-primaryBlueEnd"
-        >
-          <h2 className="text-xl lg:text-2xl 2xl:text-3xl font-semibold">
-            Thank you for making GrizzHacks 7 happen! We couldn't do it without
-            your support.
-          </h2>
-          <h3 className="text-lg lg:text-xl 2xl:text-2xl font-base mt-4">
-            Looking to sponsor?{" "}
-            <span className="underline underline-offset-4 decoration-blackColor hover:decoration-yellow decoration-2">
-              Contact us for more details!
-            </span>
-          </h3>
-        </a>
+      <div className="hidden xl:grid grid-cols-5 grid-rows-3 gap-1.5 p-4 mt-8">
         {/* MLH. Bronze */}
         <a
           href={sponsors[0].link}
-          className="p-6 col-span-1 row-span-1 ring-2 ring-blackColor flex items-center justify-center rounded-md from-[#CD7F32]/40 via-[#CD7F32]/80 to-[#CD7F32]/40 bg-gradient-to-bl"
+          className="p-6 col-span-1 row-span-1 ring-2 ring-primary bg-bgblue flex items-center justify-center rounded-md hover:from-[#CD7F32]/80 hover:via-[#CD7F32]/40 hover:to-[#CD7F32]/80 hover:bg-gradient-to-bl"
         >
           <img
             src={`${sponsors[0].logo}`}
@@ -37,7 +22,7 @@ const SponsorGrid = () => {
         {/* OUCU. Gold */}
         <a
           href={sponsors[1].link}
-          className="p-6 col-span-2 row-span-2 ring-2 ring-blackColor flex items-center justify-center rounded-md from-[#D4AF37]/40 via-[#D4AF37]/80 to-[#D4AF37]/40 bg-gradient-to-bl"
+          className="p-6 col-span-2 row-span-2 ring-2 ring-primary bg-bgblue flex items-center justify-center rounded-md hover:from-[#D4AF37]/80 hover:via-[#D4AF37]/40 hover:to-[#D4AF37]/80 hover:bg-gradient-to-bl"
         >
           <img
             src={`${sponsors[1].logo}`}
@@ -49,7 +34,7 @@ const SponsorGrid = () => {
         {/* 365 Retail Markets. Silver */}
         <a
           href={sponsors[6].link}
-          className="p-6 col-span-2 row-span-1 ring-2 ring-blackColor flex items-center justify-center rounded-md from-[#C0C0C0]/30 via-[#C0C0C0]/70 to-[#C0C0C0]/30 bg-gradient-to-bl"
+          className="p-6 col-span-2 row-span-1 ring-2 ring-primary bg-bgblue flex items-center justify-center rounded-md hover:from-[#C0C0C0]/70 hover:via-[#C0C0C0]/30 hover:to-[#C0C0C0]/70 hover:bg-gradient-to-bl"
         >
           <img
             src={`${sponsors[6].logo}`}
@@ -58,33 +43,10 @@ const SponsorGrid = () => {
           />
         </a>
 
-        {/* StandOutStickers. Bronze */}
-        <a
-          href={sponsors[4].link}
-          className="p-6 col-span-1 row-span-1 ring-2 ring-blackColor flex items-center justify-center rounded-md from-[#CD7F32]/40 via-[#CD7F32]/80 to-[#CD7F32]/40 bg-gradient-to-bl"
-        >
-          <img
-            src={`${sponsors[4].logo}`}
-            alt={`${sponsors[4].name} Logo`}
-            className="absolute w-32"
-          />
-        </a>
-
-        {/* CyberOU. Bronze */}
-        <a
-          href={sponsors[3].link}
-          className="p-6 col-span-1 row-span-1 ring-2 ring-blackColor flex items-center justify-center rounded-md from-[#CD7F32]/40 via-[#CD7F32]/80 to-[#CD7F32]/40 bg-gradient-to-bl"
-        >
-          <img
-            src={`${sponsors[3].logo}`}
-            alt={`${sponsors[3].name} Logo`}
-            className="absolute w-40"
-          />
-        </a>
-
+        {/* Github. Bronze */}
         <a
           href={sponsors[5].link}
-          className="p-6 col-span-1 row-span-1 ring-2 ring-blackColor flex items-center justify-center rounded-md from-[#CD7F32]/40 via-[#CD7F32]/80 to-[#CD7F32]/40 bg-gradient-to-bl"
+          className="p-6 col-span-1 row-span-1 ring-2 ring-primary bg-bgblue flex items-center justify-center rounded-md hover:from-[#CD7F32]/80 hover:via-[#CD7F32]/40 hover:to-[#CD7F32]/80 hover:bg-gradient-to-bl"
         >
           <img
             src={`${sponsors[5].logo}`}
@@ -92,37 +54,11 @@ const SponsorGrid = () => {
             className="absolute w-32"
           />
         </a>
-      </div>
-
-      {/*
-            Bronze
-            hover:from-[#CD7F32]/40 hover:via-[#CD7F32]/80 hover:to-[#CD7F32]/40 hover:bg-gradient-to-bl
-
-            Silver
-            hover:from-[#C0C0C0]/30 hover:via-[#C0C0C0]/70 hover:to-[#C0C0C0]/30 hover:bg-gradient-to-bl
-
-            Gold
-            hover:from-[#D4AF37]/40 hover:via-[#D4AF37]/80 hover:to-[#D4AF37]/40 hover:bg-gradient-to-bl
-        */}
-
-      {/* TABLET SCREEN SIZE */}
-      <div className="hidden lg:grid xl:hidden grid-cols-4 grid-rows-3 gap-1.5 p-4">
-        {/* OUCU. Gold */}
-        <a
-          href={sponsors[1].link}
-          className="p-6 col-span-2 row-span-2 bg-primary ring-2 ring-primary flex items-center justify-center rounded-md hover:from-[#D4AF37]/40 hover:via-[#D4AF37]/80 hover:to-[#D4AF37]/40 hover:bg-gradient-to-bl"
-        >
-          <img
-            src={`${sponsors[1].logo}`}
-            alt={`${sponsors[1].name} Logo`}
-            className=" w-80"
-          />
-        </a>
 
         {/* People.Health. Silver */}
         <a
           href={sponsors[2].link}
-          className="p-6 col-span-2 row-span-1 bg-primary ring-2 ring-primary flex items-center justify-center rounded-md hover:from-[#C0C0C0]/30 hover:via-[#C0C0C0]/70 hover:to-[#C0C0C0]/30 hover:bg-gradient-to-bl"
+          className="p-6 col-span-2 row-span-1 ring-2 ring-primary bg-bgblue flex items-center justify-center rounded-md hover:from-[#C0C0C0]/70 hover:via-[#C0C0C0]/30 hover:to-[#C0C0C0]/70 hover:bg-gradient-to-bl"
         >
           <img
             src={`${sponsors[2].logo}`}
@@ -131,10 +67,89 @@ const SponsorGrid = () => {
           />
         </a>
 
+        {/* CyberOU. Bronze */}
+        <a
+          href={sponsors[3].link}
+          className="p-6 col-span-1 row-span-1 ring-2 ring-primary bg-bgblue flex items-center justify-center rounded-md hover:from-[#CD7F32]/80 hover:via-[#CD7F32]/40 hover:to-[#CD7F32]/80 hover:bg-gradient-to-bl"
+        >
+          <img
+            src={`${sponsors[3].logo}`}
+            alt={`${sponsors[3].name} Logo`}
+            className="absolute w-56"
+          />
+        </a>
+
+        {/* Thank you message */}
+        <a
+          href="mailto:grizzhacksou@gmail.com"
+          className="h-full text-blackColor bg-bgblue flex flex-col justify-center p-6 col-span-3 row-span-1 ring-2 ring-primary rounded-md hover:bg-gradient-to-bl hover:from-primaryBlueEnd/90 hover:via-primaryBlueStart/50 hover:to-primaryBlueEnd/90"
+        >
+          <h2 className="text-center text-xl lg:text-2xl 2xl:text-3xl font-semibold">
+            Thank you for making GrizzHacks 7 happen! We couldn't do it without
+            your support.
+          </h2>
+          <h3 className="text-center text-lg lg:text-xl 2xl:text-2xl font-base mt-4">
+            Looking to sponsor?{" "}
+            <span className="underline underline-offset-4 decoration-blackColor hover:decoration-faqColor decoration-2">
+              Contact us for more details!
+            </span>
+          </h3>
+        </a>
+
+        {/* StandOutStickers. Bronze */}
+        <a
+          href={sponsors[4].link}
+          className="p-6 col-span-1 row-span-1 ring-2 ring-primary bg-bgblue flex items-center justify-center rounded-md hover:from-[#CD7F32]/80 hover:via-[#CD7F32]/40 hover:to-[#CD7F32]/80 hover:bg-gradient-to-bl"
+        >
+          <img
+            src={`${sponsors[4].logo}`}
+            alt={`${sponsors[4].name} Logo`}
+            className="absolute w-32"
+          />
+        </a>
+      </div>
+
+      {/*
+            Bronze
+            hover:from-[#CD7F32]/80 hover:via-[#CD7F32]/40 hover:to-[#CD7F32]/80 hover:bg-gradient-to-bl
+
+            Silver
+            hover:from-[#C0C0C0]/70 hover:via-[#C0C0C0]/30 hover:to-[#C0C0C0]/70 hover:bg-gradient-to-bl
+
+            Gold
+            hover:from-[#D4AF37]/80 hover:via-[#D4AF37]/40 hover:to-[#D4AF37]/80 hover:bg-gradient-to-bl
+        */}
+
+      {/* TABLET SCREEN SIZE */}
+      <div className="hidden lg:grid xl:hidden grid-cols-4 grid-rows-3 gap-1.5 p-4">
+        {/* OUCU. Gold */}
+        <a
+          href={sponsors[1].link}
+          className="p-6 col-span-2 row-span-2 bg-bgblue ring-2 ring-primary flex items-center justify-center rounded-md hover:from-[#D4AF37]/80 hover:via-[#D4AF37]/40 hover:to-[#D4AF37]/80 hover:bg-gradient-to-bl"
+        >
+          <img
+            src={`${sponsors[1].logo}`}
+            alt={`${sponsors[1].name} Logo`}
+            className=" w-80"
+          />
+        </a>
+
+        {/* 365 Retail Markets. Silver */}
+        <a
+          href={sponsors[6].link}
+          className="p-6 col-span-2 row-span-1 bg-bgblue ring-2 ring-primary flex items-center justify-center rounded-md hover:from-[#C0C0C0]/70 hover:via-[#C0C0C0]/30 hover:to-[#C0C0C0]/70 hover:bg-gradient-to-bl"
+        >
+          <img
+            src={`${sponsors[6].logo}`}
+            alt={`${sponsors[6].name} Logo`}
+            className="absolute w-80"
+          />
+        </a>
+
         {/* MLH. Bronze */}
         <a
           href={sponsors[0].link}
-          className="p-6 col-span-1 row-span-1 bg-primary ring-2 ring-primary flex items-center justify-center rounded-md hover:from-[#CD7F32]/40 hover:via-[#CD7F32]/80 hover:to-[#CD7F32]/40 hover:bg-gradient-to-bl"
+          className="p-6 col-span-1 row-span-1 bg-bgblue ring-2 ring-primary flex items-center justify-center rounded-md hover:from-[#CD7F32]/80 hover:via-[#CD7F32]/40 hover:to-[#CD7F32]/80 hover:bg-gradient-to-bl"
         >
           <img
             src={`${sponsors[0].logo}`}
@@ -146,7 +161,7 @@ const SponsorGrid = () => {
         {/* GITHUB BRONZE */}
         <a
           href={sponsors[5].link}
-          className="p-6 col-span-1 row-span-1 bg-primary ring-2 ring-primary flex items-center justify-center rounded-md hover:from-[#CD7F32]/40 hover:via-[#CD7F32]/80 hover:to-[#CD7F32]/40 hover:bg-gradient-to-bl"
+          className="p-6 col-span-1 row-span-1 bg-bgblue ring-2 ring-primary flex items-center justify-center rounded-md hover:from-[#CD7F32]/80 hover:via-[#CD7F32]/40 hover:to-[#CD7F32]/80 hover:bg-gradient-to-bl"
         >
           <img
             src={`${sponsors[5].logo}`}
@@ -158,7 +173,7 @@ const SponsorGrid = () => {
         {/* CyberOU. Bronze */}
         <a
           href={sponsors[3].link}
-          className="p-6 col-span-1 row-span-1 bg-primary ring-2 ring-primary flex items-center justify-center rounded-md hover:from-[#CD7F32]/40 hover:via-[#CD7F32]/80 hover:to-[#CD7F32]/40 hover:bg-gradient-to-bl"
+          className="p-6 col-span-1 row-span-1 bg-bgblue ring-2 ring-primary flex items-center justify-center rounded-md hover:from-[#CD7F32]/80 hover:via-[#CD7F32]/40 hover:to-[#CD7F32]/80 hover:bg-gradient-to-bl"
         >
           <img
             src={`${sponsors[3].logo}`}
@@ -170,7 +185,7 @@ const SponsorGrid = () => {
         {/* StandOutStickers. Bronze */}
         <a
           href={sponsors[4].link}
-          className="p-6 col-span-1 row-span-1 bg-primary ring-2 ring-primary flex items-center justify-center rounded-md hover:from-[#CD7F32]/40 hover:via-[#CD7F32]/80 hover:to-[#CD7F32]/40 hover:bg-gradient-to-bl"
+          className="p-6 col-span-1 row-span-1 bg-bgblue ring-2 ring-primary flex items-center justify-center rounded-md hover:from-[#CD7F32]/80 hover:via-[#CD7F32]/40 hover:to-[#CD7F32]/80 hover:bg-gradient-to-bl"
         >
           <img
             src={`${sponsors[4].logo}`}
@@ -182,7 +197,7 @@ const SponsorGrid = () => {
         {/* THANK YOU */}
         <a
           href="mailto:grizzhacksou@gmail.com"
-          className=" text-blackColor bg-primary flex flex-col items-center p-6 col-span-2 row-span-1 ring-2 ring-primary rounded-md hover:bg-gradient-to-bl hover:from-primaryBlueEnd hover:via-primaryBlueStart hover:to-primaryBlueEnd"
+          className=" text-blackColor bg-bgblue flex flex-col items-center p-6 col-span-2 row-span-1 ring-2 ring-primary rounded-md hover:bg-gradient-to-bl hover:from-primaryBlueEnd/90 hover:via-primaryBlueStart/50 hover:to-primaryBlueEnd/90"
         >
           <h2 className="text-xl lg:text-2xl 2xl:text-3xl font-semibold">
             Thank you for making GrizzHacks 7 happen! We couldn't do it without
@@ -190,7 +205,7 @@ const SponsorGrid = () => {
           </h2>
           <h3 className="text-lg lg:text-xl 2xl:text-2xl font-base mt-4">
             Looking to sponsor?{" "}
-            <span className="underline underline-offset-4 decoration-blackColor hover:decoration-yellow decoration-2">
+            <span className="underline underline-offset-4 decoration-blackColor hover:decoration-faqColor decoration-2">
               Contact us for more details!
             </span>
           </h3>
@@ -202,7 +217,7 @@ const SponsorGrid = () => {
         {/* Gold */}
         <a
           href={sponsors[1].link}
-          className="h-[20vh] p-6 bg-primary ring-2 ring-primary flex items-center justify-center rounded-md hover:from-[#D4AF37]/40 hover:via-[#D4AF37]/80 hover:to-[#D4AF37]/40 hover:bg-gradient-to-bl"
+          className="h-[20vh] p-6 bg-bgblue ring-2 ring-primary flex items-center justify-center rounded-md hover:from-[#D4AF37]/80 hover:via-[#D4AF37]/40 hover:to-[#D4AF37]/80 hover:bg-gradient-to-bl"
         >
           <img
             src={`${sponsors[1].logo}`}
@@ -213,14 +228,14 @@ const SponsorGrid = () => {
 
         {/* Grid containing both a silver item and a bronze grid*/}
         <div className=" grid grid-cols-2 gap-2 w-full">
-          {/* People.Health */}
+          {/* 365 Retail Markets */}
           <a
-            href={sponsors[2].link}
-            className="h-[20vh] col-span-1 p-6 bg-primary ring-2 ring-primary flex items-center justify-center rounded-md hover:from-[#C0C0C0]/30 hover:via-[#C0C0C0]/70 hover:to-[#C0C0C0]/30 hover:bg-gradient-to-bl"
+            href={sponsors[6].link}
+            className="h-[20vh] col-span-1 p-6 bg-bgblue ring-2 ring-primary flex items-center justify-center rounded-md hover:from-[#C0C0C0]/70 hover:via-[#C0C0C0]/30 hover:to-[#C0C0C0]/70 hover:bg-gradient-to-bl"
           >
             <img
-              src={`${sponsors[2].logo}`}
-              alt={`${sponsors[2].name} Logo`}
+              src={`${sponsors[6].logo}`}
+              alt={`${sponsors[6].name} Logo`}
               className="absolute w-60"
             />
           </a>
@@ -229,7 +244,7 @@ const SponsorGrid = () => {
             {/* MLH */}
             <a
               href={sponsors[0].link}
-              className="h-[20vh] md:p-4 p-2 col-span-1 row-span-1 bg-primary ring-2 ring-primary flex items-center justify-center rounded-md hover:from-[#CD7F32]/40 hover:via-[#CD7F32]/80 hover:to-[#CD7F32]/40 hover:bg-gradient-to-bl"
+              className="h-[20vh] md:p-4 p-2 col-span-1 row-span-1 bg-bgblue ring-2 ring-primary flex items-center justify-center rounded-md hover:from-[#CD7F32]/80 hover:via-[#CD7F32]/40 hover:to-[#CD7F32]/80 hover:bg-gradient-to-bl"
             >
               <img
                 src={`${sponsors[0].logo}`}
@@ -240,7 +255,7 @@ const SponsorGrid = () => {
             {/* GITHUB */}
             <a
               href={sponsors[5].link}
-              className="h-[20vh] md:p-4 p-2 col-span-1 row-span-1 bg-primary ring-2 ring-primary flex items-center justify-center rounded-md hover:from-[#CD7F32]/40 hover:via-[#CD7F32]/80 hover:to-[#CD7F32]/40 hover:bg-gradient-to-bl"
+              className="h-[20vh] md:p-4 p-2 col-span-1 row-span-1 bg-bgblue ring-2 ring-primary flex items-center justify-center rounded-md hover:from-[#CD7F32]/80 hover:via-[#CD7F32]/40 hover:to-[#CD7F32]/80 hover:bg-gradient-to-bl"
             >
               <img
                 src={`${sponsors[5].logo}`}
@@ -268,7 +283,7 @@ const SponsorGrid = () => {
           {/* CyberOU */}
           <a
             href={sponsors[3].link}
-            className="h-[20vh] md:p-4 p-2 col-span-1 row-span-1 bg-primary ring-2 ring-primary flex items-center justify-center rounded-md hover:from-[#CD7F32]/40 hover:via-[#CD7F32]/80 hover:to-[#CD7F32]/40 hover:bg-gradient-to-bl"
+            className="h-[20vh] md:p-4 p-2 col-span-1 row-span-1 bg-bgblue ring-2 ring-primary flex items-center justify-center rounded-md hover:from-[#CD7F32]/80 hover:via-[#CD7F32]/40 hover:to-[#CD7F32]/80 hover:bg-gradient-to-bl"
           >
             <img
               src={`${sponsors[3].logo}`}
@@ -280,7 +295,7 @@ const SponsorGrid = () => {
           {/* STAND OUT */}
           <a
             href={sponsors[4].link}
-            className="h-[20vh] md:p-4 p-2 col-span-1 row-span-1 bg-primary ring-2 ring-primary flex items-center justify-center rounded-md hover:from-[#CD7F32]/40 hover:via-[#CD7F32]/80 hover:to-[#CD7F32]/40 hover:bg-gradient-to-bl"
+            className="h-[20vh] md:p-4 p-2 col-span-1 row-span-1 bg-bgblue ring-2 ring-primary flex items-center justify-center rounded-md hover:from-[#CD7F32]/80 hover:via-[#CD7F32]/40 hover:to-[#CD7F32]/80 hover:bg-gradient-to-bl"
           >
             <img
               src={`${sponsors[4].logo}`}
@@ -291,7 +306,7 @@ const SponsorGrid = () => {
 
           <a
             href="mailto:grizzhacksou@gmail.com"
-            className=" text-blackColor bg-primary flex flex-col items-center justify-center p-3 col-span-2 row-span-1 ring-2 ring-primary rounded-md hover:bg-gradient-to-bl hover:from-primaryBlueEnd hover:via-primaryBlueStart hover:to-primaryBlueEnd"
+            className=" text-blackColor bg-bgblue flex flex-col items-center justify-center p-3 col-span-2 row-span-1 ring-2 ring-primary rounded-md hover:bg-gradient-to-bl hover:from-primaryBlueEnd/90 hover:via-primaryBlueStart/50 hover:to-primaryBlueEnd/90"
           >
             <h2 className="md:text-lg text-base font-semibold">
               Thank you for making GrizzHacks 7 happen! We couldn't do it
@@ -299,7 +314,7 @@ const SponsorGrid = () => {
             </h2>
             <h3 className="text-base mt-2">
               Looking to sponsor?{" "}
-              <span className="underline underline-offset-4 decoration-blackColor hover:decoration-yellow decoration-2">
+              <span className="underline underline-offset-4 decoration-blackColor hover:decoration-faqColor decoration-2">
                 Contact us for more details!
               </span>
             </h3>
