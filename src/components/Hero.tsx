6d380fdd1ca64be875306255ac93e9949faa549a
HououinKyouma555 <130073365+HououinKyouma555@users.noreply.github.com>
2025-09-29 21:44:58 -0400

    Change GH 7 to GH 8

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -22,14 +22,14 @@ const Hero = () => {
         <section className="hidden md:block">
           <div className="flex flex-col lg:mt-12 mt-16 2xl:mt-20 items-center">
             <h1 className="text-7xl lg:text-8xl 2xl:text-9xl font-bold bg-gradient-to-b from-blackStart to-blackEnd bg-clip-text text-transparent">
-              GrizzHacks 7
+              GrizzHacks 8
             </h1>
           </div>
           <div className="flex gap-10 mt-4 2xl:mb-10 mb-12 justify-center">
             <div className="flex gap-2 items-center justify-center">
               <img className="w-8 2xl:w-10" src="/date.svg" alt="" />
               <h2 className="text-2xl 2xl:text-3xl text-blackColor">
-                March 22nd-23rd
+                March 21st-22nd
               </h2>
             </div>
             <div className="flex items-center justify-center gap-1">
@@ -45,7 +45,7 @@ const Hero = () => {
               target="_blank"
               className="flex items-center justify-center py-2 2xl:py-3 px-4 2xl:px-6 text-2xl lg:text-3xl rounded-2xl bg-primary hover:bg-primaryHover w-fit text-white font-semibold font-montserrat relative ring-2 ring-primaryHover"
             >
-              Photo Gallery
+              GH 7 Gallery
             </a>
             <h3 className="flex text-base 2xl:text-lg text-blackColor">
               The Quackathon Has Ended
@@ -65,13 +65,13 @@ const Hero = () => {
         <section className="md:hidden w-full">
           <div className="flex flex-col items-center justify-center mt-16">
             <h1 className="text-5xl font-bold bg-gradient-to-b from-blackStart to-blackEnd bg-clip-text text-transparent">
-              GrizzHacks 7
+              GrizzHacks 8
             </h1>
           </div>
           <div className="font-montserrat flex flex-col gap-1 mb-12 mt-4 items-center justify-center text-base font-semibold text-blackColor ">
             <div className="flex gap-1 items-center">
               <img className="w-5" src="/date.svg" alt="" />
-              <h2>March 22nd-23rd</h2>
+              <h2>March 21st-22nd</h2>
             </div>
             <div className="flex gap-0 items-center">
               <img className="w-5" src="/pin.png" alt="" />
@@ -83,7 +83,7 @@ const Hero = () => {
               href="https://ethantocco.picflow.com/grizzhacks2025"
               className="font-montserrat flex items-center justify-center p-2 px-3 text-xl lg:text-3xl 2xl:text-3xl rounded-xl bg-primary hover:bg-primaryHover w-fit text-white font-semibold relative ring-2 ring-primaryHover"
             >
-              Photo Gallery
+              GH 7 Gallery
             </a>
             <h3 className="flex text-base font-montserrat">
               The Quackathon Has Ended
